f36d7c0dc68f476703f7a8485faae489b0641984
JGuilherme.aa <joaoguilherme.aa1603@gmail.com>
2025-01-22 20:51:15 -0300

    Pedro update

--- script2.js
+++ script2.js
@@ -8,6 +8,20 @@ document.addEventListener("DOMContentLoaded", function()
     
     body = document.getElementsByTagName("body")[0];
     userImage = document.querySelector("#userImage")
+    function normalui()
+    {
+        let principal = document.querySelector(".principal");
+        let sites = document.querySelector(".sites");
+
+        principal.style.position = "relative";
+        principal.style.bottom = "";
+        principal.style.flexDirection = "column";
+        principal.style.height = "20vh";
+
+        sites.style.position = "";
+        sites.style.top = "";
+    }
+
     function user(userid)
     {
         switch (userid)
@@ -15,11 +29,23 @@ document.addEventListener("DOMContentLoaded", function()
             case '0': /*Guilherme*/
             document.body.style.backgroundImage = "url(images/wallpaperg.jpg)"; 
             userImage.src = "images/userg.jpg"
+            normalui();
             break;
 
             case '1': /*Pedro*/
+            let principal = document.querySelector(".principal");
+            let sites = document.querySelector(".sites");
+
+            principal.style.position = "absolute";
+            principal.style.bottom = "0%";
+            principal.style.flexDirection = "column-reverse";
+            principal.style.height = "15vh";
+
+            sites.style.position = "fixed";
+            sites.style.top = "0%";
             document.body.style.backgroundImage = "url(images/wallpaperp.png)"; 
             userImage.src = "images/userp.png"
+            
             break;
         }   
         localStorage.setItem("userid", userid);
